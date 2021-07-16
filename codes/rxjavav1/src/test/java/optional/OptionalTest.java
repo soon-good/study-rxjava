f31b58fd3b96d906dc925bf550c9bb6879431723
soon.good <playmymind@daum.net>
2021-07-16 09:26:13 +0900

    패키지 위치 변경

--- codes/rxjavav1/src/test/java/optional/OptionalTest.java
+++ codes/rxjavav1/src/test/java/optional/OptionalTest.java
@@ -1,2 +1,32 @@
-package optional;public class OptionalTest {
+package optional;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+
+public class OptionalTest {
+
+	public class Employee{
+		private String name;
+		public Employee (String name){
+			this.name = name;
+		}
+		public String getName() {
+			return name;
+		}
+	}
+
+	@Test
+	public void OptionalMap_of_map함수_테스트(){
+		Employee e1 = new Employee("지드래곤");
+		Optional<String> name = Optional.of(e1).map(Employee::getName);
+		System.out.println(name);
+	}
+
+	@Test
+	public void OptionalMap_ofNullable_map함수_테스트(){
+		Optional<Employee> optEmployee = Optional.ofNullable(null);
+		Optional<String> optName = optEmployee.map(Employee::getName);
+		System.out.println("optName = " + optName);
+	}
 }
